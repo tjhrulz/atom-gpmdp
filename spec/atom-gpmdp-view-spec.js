9e72b9b0b5b08293f871d5a37a1d93c1e1da9fa0
Trevor Hamilton <tjh1003@sru.edu>
2017-02-17 23:25:01 -0500

    Initial commit
    Most of the code is still the autogen code, I have some untested
    websocket code however I dont know much about debugging atom plugins so
    not sure if it is connecting yet.

---
+++ spec/atom-gpmdp-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGpmdpView from '../lib/atom-gpmdp-view';
+
+describe('AtomGpmdpView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
